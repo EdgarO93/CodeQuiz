cad8e1351a0825900b7d506a91c2d4dad1241318
Edgar Ortega <edort93@gmail.com>
2021-08-19 11:04:30 -0500

    Fixxed quiz and added sample questions

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,60 +1,70 @@
 
 //define variables
-var timerElement= document.querySelector(".timer");
-var  intro= document.querySelector (".intro");
-var startBtnEL= document.querySelector ("#start-btn");
-var questionCount= 0;
+var timerElement = document.querySelector(".timer");
+var intro = document.querySelector(".intro");
+var startBtnEL = document.querySelector("#start-btn");
+var questionCount = 0;
 var timerCount;
 var timer;
-var rightCounter= 0;
+var rightCounter = 0;
 var wrongCounter = 0;
 // using an array for questions
 var questions = [
-{
-    "text": "Sample Question",
-    "options": ["wrong", "wrong2", "right", 9],
-    "solution": "right"
-},
-{
-    "text": "Sample Question2",
-    "options": ["wrong", "wrong2", "right", 9],
-    "solution": "right"
-},
+    {
+        "text": "Test question, what is javascript?",
+        "options": ["wrong", "wrong2", "right", 9],
+        "solution": "right"
+    },
+    {
+        "text": "Sample Question2",
+        "options": ["wrong", "wrong2", "right2", 9],
+        "solution": "right2"
+    },
 
-{
-    "text": "Sample Question3",
-    "options": ["wrong", "wrong2", "right", 9],
-    "solution": "right"
-}
+    {
+        "text": "Sample Question3",
+        "options": ["wrong", "wrong2", "right", 9],
+        "solution": "right"
+    },
+    {
+        "text": "Sample Question4",
+        "options": ["wrong", "wrong2", "right", 9],
+        "solution": "right"
+    },
 
-
+    {
+        "text": "Sample Question5",
+        "options": ["wrong", "wrong2", "right", 9],
+        "solution": "right"
+    }
 ]
 
 //function for timer, 
 //need to subtract time for a wrong question, minus 10? 
 function startTimer() {
     // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (rightCounter = 5 || timerCount >= 0) {
-        // Tests if win condition is met
-        if ( timerCount > 0) {
-          // Clears interval and stops timer
-        //   clearInterval(timer);
-        //   winGame();
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (rightCounter = 5 || timerCount >= 0) {
+            // Tests if win condition is met
+            if (timerCount > 0) {
+                // Clears interval and stops timer
+                //   clearInterval(timer);
+                //   winGame();
+            }
         }
-      }
-      // Tests if time has run out
-      if (timerCount  <=0) {
-        // Clears interval
-        timerCount= 0;
-        clearInterval(timer);
-        // loseGame(); need to call end game function
-      }
+        // Tests if time has run out
+        if (timerCount <= 0) {
+            // Clears interval
+            timerCount = 0;
+            clearInterval(timer);
+            // need to call end game function
+            endGame();
+        }
     }, 1000);
-  }
-  
+}
+
 
 //function for highscore
 //prob do a one 100 point for right
@@ -62,32 +72,32 @@ function startTimer() {
 
 //generate button , then run game function
 function displayQuestion(questions) {
-    var $text =$('<h2>').text(questions["text"]);
+    var $text = $('<h2>').text(questions["text"]);
     $(".questionSec").append($text);
 
     var opts = questions["options"];
-    for (let i=0; i<opts.length; i++) {
-        var $option = $('<button>').attr({value: opts[i],class:'questions-option'});
+    for (let i = 0; i < opts.length; i++) {
+        var $option = $('<button>').attr({ value: opts[i], class: 'questions-option' });
         $option.text(opts[i])
         $(".questionSec").append($option);
-        
+
     }
 
 
 }
 
-$(document).on("click",".questions-option", function() {
+$(document).on("click", ".questions-option", function () {
 
     //IF clicked correct , alert user then move to next question [i+]
-    if(this.value=== 'right' ){
-    console.log(this.value)
-    alert('right answer')
-    questionCount++
-    displayQuestion(questions[questionCount])
-    } else{
+    if (this.value === questions[questionCount].solution) {
+        console.log(this.value)
+        alert('right answer')
+        questionCount++
+        displayQuestion(questions[questionCount])
+    } else {
         console.log(this.value)
         alert('wrong answer, minus 10 seconds')
-        timerCount= timerCount -10;
+        timerCount = timerCount - 10;
         console.log(timerCount)
     }
     //else if clicked incorrect, alert user and subtract 10 seconds and move to next question
@@ -106,12 +116,12 @@ $(document).on("click",".questions-option", function() {
 //then invoke highscore
 
 //button for start , intro text and start button will disappear need to find way to cycle questions.
-$(startBtnEL).on("click", function(){
- displayQuestion(questions[questionCount])
- $("#start-btn").hide();
- $(".intro").hide();
- timerCount= 100;
- startTimer()
+$(startBtnEL).on("click", function () {
+    displayQuestion(questions[questionCount])
+    $("#start-btn").hide();
+    $(".intro").hide();
+    timerCount = 100;
+    startTimer()
 });
 
 
